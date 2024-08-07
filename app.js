4fdb488262fbdd1105fb26688bcf67800f4fcc54
Khedidja-Chaabane <khedidjal94@gmail.com>
2024-08-07 16:35:51 +0200

    travail sur le profil,admin, posts,navbar

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const methodOverride = require('method-override'); // on appelle la dépendance 
 app.use(methodOverride('_method')); // on utilise la dépendance method-override ; des qu'on voit un _method dans l'URL, c'est qu'on utilise la methode override
 var Forum = require('./models/Forum');
 var User = require('./models/User');
+
 //PARTIE FORUM
 
 // Création d'un nouveu post
@@ -32,14 +33,14 @@ app.post('/newPost', function (req, res) {
         titre: req.body.titre,
         auteur: req.body.auteur,
         texte: req.body.texte,
-        images: req.body.images,
+        images: req.body.images || []  // Utiliser un tableau vide si aucune image n'est fournie
     })
-    NewPost.save()  // on enregistre les données dans la bdd 
+    NewPost.save()  
         .then(() => {
             console.log("Post saved !");
-            res.redirect('/forum');          // on rederige vers la page blog
+            res.redirect('http://localhost:3000/forum');         
         })
-        .catch(error => console.log(error)); // on affiche l'erreur 
+        .catch(error => console.log(error)); 
 });
 
 // récupération de tous les posts
@@ -52,6 +53,34 @@ app.get('/forum', function (req, res) {
         .catch(error => console.log(error));
 });
 
+// Affichage d'un post
+app.get('/post/:id', function (req, res) {
+    Forum.findOne({
+        _id: req.params.id
+    }).then(post => {
+        res.json(post);
+    })
+        .catch(error => console.log(error));
+});
+
+//modification d'un post
+app.put('/updatePost/:id', function (req, res) {
+    const Post = {
+        titre: req.body.titre,
+        auteur: req.body.auteur,
+        texte: req.body.texte,
+        images: req.body.images || []
+    }
+    //matching data: on fait un matching entre les id des données dans la bdd et celles presentes dans l'url
+    Forum.updateOne({
+        _id: req.params.id           // on récupère l'id de la donnée
+    }, { $set: Post })                  // avec $set on met à jour les données
+        .then((data) => {
+            console.log("Post updated !");
+            res.redirect(`http://localhost:3000/showPost/${req.params.id}`);
+        })
+        .catch(error => console.log(error));
+});
 //INSCRIPTION
 
 //NEW USER
@@ -64,7 +93,7 @@ app.post('/api/newUser', function (req, res) {
     })
     Data.save().then(()=>{
         console.log('User saved successfully');
-        res.redirect('http://localhost:3000/')
+        res.redirect('http://localhost:3000/connexion')
     })
     .catch((error) => console.log(error));
 });
@@ -95,14 +124,37 @@ app.post('/api/connexion', function (req, res) {
             }
 
             else {
-                return res.redirect('http://localhost:3000/profile/:id');
+                return res.redirect(`http://localhost:3000/profile/${user._id}`);
             }
 
         })
         .catch(error => console.log(error));
 });
 
+//Profile
 
+app.get('/user/:id', function (req, res) {
+    User.findOne({
+        _id: req.params.id        // on fait un matching entre l'id de la donnée et l'id de l'url
+    }).then(data => {
+        res.json(data); 
+    })
+        .catch(error => console.log(error));
+});
+
+//Déconnexion
+app.get('/logout', function(req, res) {
+    res.clearCookie("access-token");
+    res.redirect('http://localhost:3000/');
+});
+
+// Gestion des users par l'admin
+app.get('/admin/gestionUsers', function (req, res) {
+    User.find().then(users => {
+        res.json(users);
+    })
+        .catch(error => console.log(error));
+});
 var server = app.listen(5000, function () {   // on lance le serveur sur le port 5000
     console.log('server running on port 5000');    //mettre un message dans la console quand le serveur est lancé
 
